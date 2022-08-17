09f64cbf75d2e461d14d1d0a08de9030ecfbf202
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-08-17 17:00:15 -0400

    Add delete and filter task functionality

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ function loadEventListeners(){
   form.addEventListener('submit', addTask)
   // Remove task event
   taskList.addEventListener('click', removeTask)
+  // Clear task list event
+  clearBtn.addEventListener('click', clearTasks)
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks)
 }
 
 function addTask(e){
@@ -46,6 +50,29 @@ function addTask(e){
 // Remove task
 function removeTask(e){
   if (e.target.parentElement.classList.contains('delete-item')) {
-    e.target.parentElement.parentElement.remove()
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove()
+    }
   }
+}
+
+// Clear task list
+function clearTasks(){
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild)
+  }
+}
+
+// Filter tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase()
+
+  document.querySelectorAll('.collection-item').forEach(function (task){
+    const item = task.firstChild.textContent
+    if (item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block'
+    } else {
+      task.style.display = 'none'
+    }
+  })
 }
